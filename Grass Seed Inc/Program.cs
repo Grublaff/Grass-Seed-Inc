a22696ee4de5b1fcb5890ad314da633842463c41
Grubla <filip.aburg@gmail.com>
2021-04-13 20:10:09 +0200

    Add project files.

---
+++ Grass Seed Inc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grass_Seed_Inc{
+    class Program{
+        static void Main(){
+            double cost = Double.Parse(Console.ReadLine());
+            double lawnToSow = Double.Parse(Console.ReadLine());
+            double total = 0;
+            for (int i = 0; i < lawnToSow; i++){
+                String[] split = Console.ReadLine().Split(" ");
+                total += Double.Parse(split[0]) * Double.Parse(split[1]) * cost;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
